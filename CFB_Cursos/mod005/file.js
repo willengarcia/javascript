c66131b372b428eec5fd05ab13926328587ba1b0
Willen Garcia <89990936+willengarcia@users.noreply.github.com>
2024-06-24 11:32:00 -0300

    sla

---
+++ CFB_Cursos/mod005/file.js
@@ -0,0 +1,4 @@
+const cursos = ['java', 'css', 'php', 'arduino']
+const carros = ['Polo', 'Golf', 'Ônix', 'Chronos']
+
+export {cursos, carros} // Comando para exportar
---
+++ CFB_Cursos/mod005/script.js
@@ -0,0 +1,2 @@
+import { cursos, carros} from "./file.js" // Comando para importar
+console.log(cursos, carros)
