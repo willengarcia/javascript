327bd88567a1d5a9d1cce8c2417da9adc3c5a114
Willen Garcia <89990936+willengarcia@users.noreply.github.com>
2024-07-27 12:43:24 -0300

    Fianlização do DataGridView

--- CFB_Cursos/mod009/datagridview/script.js
+++ CFB_Cursos/mod009/datagridview/script.js
@@ -1,4 +1,7 @@
 const divDatagrid = document.querySelector('.dados')
+const botaoFechar = document.querySelector('.fechar')
+const botaoGravar = document.querySelector('.gravar')
+const botaoCancelar = document.querySelector('.cancelar')
 const config  = {
     endpoint:'http://localhost:1880/produtos'
 }
@@ -62,10 +65,26 @@ function pegaId(el){
         if(tipo == 'delete.png'){
             apagar(id)
             linhaSelecionada.remove()
-        }else if(tipo =='editar.png'){
-            console.log('Atualizar')
+        }else if(tipo =='editar.png'){  
+            const editar = document.querySelector('.editar')
+            editar.classList.add('ativo')
+            view(id)
+            botaoCancelar.addEventListener('click', (evt)=>{
+                editar.classList.remove('ativo')
+            })
+            botaoGravar.addEventListener('click', (evt)=>{
+                editarProduto(id)
+                setInterval(()=>{
+                    location.reload()
+                }, 1000)
+            })
         }else if(tipo =='visualizar.png'){
-            console.log('Visualizar')
+            view(id)
+            const janela = document.querySelector('.janela')
+            janela.classList.add('ativo')
+            botaoFechar.addEventListener('click', (evt)=>{
+                janela.classList.remove('ativo')
+            })
         }
     })
 }
@@ -77,6 +96,37 @@ function apagar(id){
             console.log('Removido com sucesso')
         }
     })
+    .catch(erro=>{
+        console.log(erro)
+    })
+}
+function view(id){
+    const inputs = [...document.querySelectorAll('input')]
+    const endpoint = `http://localhost:1880/selecionaproduto/${id}`
+    fetch(endpoint)
+    .then(res=>res.json())
+    .then(res=>{
+        res.forEach((dados)=>{
+            inputs[0].value = dados.id
+            inputs[1].value = dados.produto
+            inputs[2].value = dados.marca
+            inputs[3].value = dados.modelo
+
+            document.querySelector('#idEditar').innerHTML = `ID: ${dados.id}`
+            inputs[4].value = dados.produto
+            inputs[5].value = dados.marca
+            inputs[6].value = dados.modelo
+        })
+    })
+    .catch(erro=>{
+        console.log(erro)
+    })
+}
+function editarProduto(id){
+    const inputs = [...document.querySelectorAll('.editar input')]
+    const endpoint = `http://localhost:1880/editaproduto/${id}/${inputs[0].value}/${inputs[1].value}/${inputs[2].value}`
+    fetch(endpoint)
+    .then(res=>res.json())
     .catch(erro=>{
         console.log(erro)
     })
